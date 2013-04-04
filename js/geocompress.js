02895776ac59441906ed87480d9ffe8aa05e6129
Jeffrey Rackauckas <Jeffreyrack@gmail.com>
2013-04-04 11:24:49 -0700

    Fixed some bugs with editting additional answers

--- js/geocompress.js
+++ js/geocompress.js
@@ -165,31 +165,14 @@ function submitLatLng(location) {
 		} else{
 			// TODO: Find a way to check if additional questions exist.
 			document.getElementById("activity").innerHTML=multiple;
-			var hunt=JSON.parse(sessionStorage.hunts)[0];
-			if(hunt.additionalQuestions) var additionalQuestions=JSON.parse(hunt.additionalQuestions);
-			else var additionalQuestions="";
 			var activity=JSON.parse(sessionStorage.activity);
 			var additionalAnswers={answera:activity.optionalAnswer1, answerb:activity.optionalAnswer2, answerc:activity.optionalAnswer3};
-			// Deal with the first additional questions.
-			if(!additionalQuestions.questiona){
-				document.getElementsByName("optionalQuestion1")[0].style.display="none";
-				document.getElementsByName("optionalAnswer1")[0].style.display="none";
-			} else if(additionalAnswers.answera)
-				document.getElementsByName("optionalAnswer1")[0].innerHTML=additionalAnswers.answera;
-			
-			// Deal with the second additional question.
-			if(!additionalQuestions.questionb){
-				document.getElementsByName("optionalQuestion2")[0].style.display="none";
-				document.getElementsByName("optionalAnswer2")[0].style.display="none";
-			} else if(additionalAnswers.answerb)
-				document.getElementsByName("optionalAnswer2")[0].innerHTML=additionalAnswers.answerb;
-			
-			// Deal with the third question.
-			if(!additionalQuestions.questionc){
-				document.getElementsByName("optionalQuestion3")[0].style.display="none";
-				document.getElementsByName("optionalAnswer3")[0].style.display="none";
-			} else if(additionalAnswers.answerc)
-				document.getElementsByName("optionalAnswer3")[0].innerHTML=additionalAnswers.answerc;
+			document.getElementsByName("interesting_url")[0].innerHTML=activity.interesting_url;
+			document.getElementsByName("partner_names")[0].innerHTML=activity.partner_names;
+			// While the next function is going to have to parse the function, it provides more usability if this function is passed the JSON encoded string.
+			// My reasoning for this is that when the additionalAnswers are taken from an activity, they will already be JSON encoded.
+			// This should be the ONLY case where additionalAnswers aren't JSON encoded by default.
+			displayAdditionalQuestions(JSON.stringify(additionalAnswers));
 		}
 	} else{
 		// TODO: How to handle the user not submitting local storage?
